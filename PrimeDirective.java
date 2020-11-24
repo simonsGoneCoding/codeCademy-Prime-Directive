7265c7af0e797be5fd4b0ba40132dfd44a8a9349
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-24 15:33:29 +0000

    Create onlyPrimes method taking an Array as argument and returning ArrayList of only prime numbers.

--- PrimeDirective.java
+++ PrimeDirective.java
@@ -18,16 +18,22 @@ class PrimeDirective {
         return true;
     }
 
+    public ArrayList<Integer> onlyPrimes(int[] numbers) {
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+
+        for (int number : numbers) {
+            if (isPrime(number)) {
+                primes.add(number);
+            }
+        }
+        return primes;
+    }
+
     public static void main(String[] args) {
 
         PrimeDirective pd = new PrimeDirective();
         int[] numbers = { 6, 29, 28, 33, 11, 100, 101, 43, 89 };
 
-        System.out.println(pd.isPrime(28));
-        System.out.println(pd.isPrime(7));
-        System.out.println(pd.isPrime(3));
-        System.out.println(pd.isPrime(1));
-        System.out.println(pd.isPrime(2));
-        System.out.println(pd.isPrime(0));
+        System.out.println(pd.onlyPrimes(numbers));
     }
 }
